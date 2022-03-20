fce780090903635409c04cda83a4e1c6b7c2b2aa
trulybright <trulybright@yonsei.ac.kr>
2022-03-21 04:48:55 +0900

    let's build from scratch

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace Example
+{
+    public class Chat : WebSocketBehavior
+    {
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            Sessions.Broadcast(e.Data);
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var wssv = new WebSocketServer("ws://localhost:8000");
+
+            wssv.AddWebSocketService<Chat>("/");
+            wssv.Start();
+            Console.ReadKey(true);
+            wssv.Stop();
+        }
+    }
+}
